8d181c803439fd0ad5e8de2177bb49a2906b6497
DomenicoBarb <domenicobarberi585@gmail.com>
2023-01-09 00:16:52 -0500

    further commnts for code clarity

--- script.js
+++ script.js
@@ -32,26 +32,27 @@ function writePassword() {
 function getPrompts() {
   // resets choice array (defaults to empty)
   choiceArr = [];
-    // password length 8 - 128 prompt
+  
+  // password length 8 - 128 prompt
   characterLength = parseInt(prompt("How many characters would you like your new password to be? (type 8 - 128)"));
-    // if character number selection is correct 
+    // if character number selection is not a number (NaN) OR less than 8 OR more than 128 return false
     if(isNaN(characterLength) || characterLength < 8 || characterLength > 128 ) {
       alert("Character length has to be between 8 - 128 digits. Please try again and type the number, do NOT spell it. (Ex: 12 NOT 'twelve')");
       return false;
     }
-    // Lowercase selection (if yes it adds lowerCaseArr to choiceArr)
+    // Lowercase selection (if yes it adds lowerCaseArr to choiceArr, if no nothing is changed)
     if (confirm("Would you like lowercase letters in the password?")) {
       choiceArr = choiceArr.concat(lowerCaseArr);
     }
-    // Uppercase selection (if yes it adds upperCaseArr to choiceArr)
+    // Uppercase selection (if yes it adds upperCaseArr to choiceArr, if no nothing is changed)
     if (confirm("Would you like uppercase letters in the password?")) {
       choiceArr = choiceArr.concat(upperCaseArr);
     }
-    // Special characters selection (if yes it adds specialCharArr to choiceArr)
+    // Special characters selection (if yes it adds specialCharArr to choiceArr, if no nothing is changed)
     if (confirm("Would you like special characters in the password? (Ex: #, %, *, @)")) {
       choiceArr = choiceArr.concat(specialCharArr);
     }
-    // Numbers selection (if yes it adds numberArr to choiceArr)
+    // Numbers selection (if yes it adds numberArr to choiceArr, if no nothing is changed)
     if (confirm("Would you like numbers in the password?")) {
       choiceArr = choiceArr.concat(numberArr);
     }
@@ -60,13 +61,13 @@ function getPrompts() {
 
 // Step 5 (Password Generation)
 function generatePassword() {
-  // resets password value (defaults to an empty string)
+  // resets password value (defaults to an empty string to clear pass attempts)
   let password = "";
-  // loop will run until i = the same value as characterLength
+  // loop will run until i = the same value as characterLength (allows for repeat attempts without error, resetting to i = 0 each time)(this sequentially generates each character until done)
   for(let i = 0; i < characterLength; i++) {
-    // random character selection math, math.floor for an integer (math.random creates random floating point value) giving random value between selected length
+    // random character selection math, create a variable named randomIndex, math.floor to make it an integer, math.random multiplied by choiceArr.length creates a random number within the range designated by choiceArr.length
     let randomIndex = Math.floor(Math.random() * choiceArr.length);
-    // starts with default password value and adds new randomIndex (random symbol from that array)
+    // starts with default password value (an empty string) and adds new randomIndex (at this point, the completed password meeting inputed criteria)
     password = password + choiceArr[randomIndex];
   }
   // display generated password

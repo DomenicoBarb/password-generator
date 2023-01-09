8180ccbcc7d0adf7f8d28b5f936960ebee05aa10
DomenicoBarb <domenicobarberi585@gmail.com>
2023-01-08 20:17:04 -0500

    started JS code

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // Assignment code here
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  // button click confirmation
+  console.log("The button has been clicked.");
+  // Prompt the user for the password criteria
+  // 1. password length 8 - 128
+  // 2. Lowercase, uppercase, special characters
+  // Validate the input, ensure char limit is between 8 - 128
+  // Generate password
+  
+  // Display generated password
+  return "Generated password will go here!"
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();

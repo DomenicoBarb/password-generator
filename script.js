9b5a28a35ca4fbe0c4ea0cd41bd9437bc601dd74
DomenicoBarb <domenicobarberi585@gmail.com>
2023-01-08 20:34:45 -0500

    added arrays for password content

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
+var characterLength = 8;
+var choiceArr = [];
+
+var specialCharArr = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '{', '}', '[', ']', '_', '-', '=', '+', '?', '/'];
+var lowerCaseArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCasearr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numberArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
 // Assignment code here
 var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 
 function generatePassword() {
   // button click confirmation
   console.log("The button has been clicked.");
   // Prompt the user for the password criteria
   // 1. password length 8 - 128
+  function getPrompt() {
+    characterLength = parseInt(prompt("How many characters would you like your new password to be? (8 - 128 characters)"))
+  }
   // 2. Lowercase, uppercase, special characters
   // Validate the input, ensure char limit is between 8 - 128
   // Generate password
@@ -22,6 +37,3 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);

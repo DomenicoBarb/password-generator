e1027ff85cd9818abdeb6b4fd976f90410a8015c
DomenicoBarb <domenicobarberi585@gmail.com>
2023-01-08 21:43:22 -0500

    changed 'var' to 'let', comments added and index.html h2 changed

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
-var characterLength = 8;
-var choiceArr = [];
+// Global Variables
+let characterLength = 8;
+let choiceArr = [];
 
-var specialCharArr = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '{', '}', '[', ']', '_', '-', '=', '+', '?', '/'];
-var lowerCaseArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var upperCaseArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var numberArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// Arrays for lower/uppercase characters, special characters and numbers
+let specialCharArr = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '{', '}', '[', ']', '_', '-', '=', '+', '?', '/'];
+let lowerCaseArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+let upperCaseArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+let numberArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-// Assignment code here
-var generateBtn = document.querySelector("#generate");
+// Generate Button
+let generateBtn = document.querySelector("#generate");
 
-// Add event listener to generate button
+// When "Clicked" run writePassword
 generateBtn.addEventListener("click", writePassword);
 
-// Write password to the password input
+// Displays new password on screen (in on-screen box)
 function writePassword() {
   // Calls getPrompts
-  var correctChoicePrompt = getPrompts(); // will return true or false
-  var passwordText = document.querySelector("#password");
+  let correctChoicePrompt = getPrompts(); // will return true or false
+  let passwordText = document.querySelector("#password");
   // if true start generatePassword function
   if (correctChoicePrompt) {
-    var newPassword = generatePassword();
+    let newPassword = generatePassword();
     passwordText.value = newPassword;
-  } else { // if false set passwordText.value to zero
+  } 
+  // if false set passwordText.value to nothing//
+  else {
     passwordText.value = "";
   }
 }
@@ -29,11 +33,11 @@ function writePassword() {
 // Password Generation
 function generatePassword() {
   // resets password value (defaults to an empty string)
-  var password = "";
+  let password = "";
   // loop will run until i = the same value as characterLength
-  for(var i = 0; i < characterLength; i++) {
+  for(let i = 0; i < characterLength; i++) {
     // random character selection math, math.floor for an integer (math.random creates random floating point value) giving random value between selected length
-    var randomIndex = Math.floor(Math.random() * choiceArr.length);
+    let randomIndex = Math.floor(Math.random() * choiceArr.length);
     // starts with default password value and adds new randomIndex (random symbol from that array)
     password = password + choiceArr[randomIndex];
   }
@@ -52,19 +56,19 @@ function getPrompts() {
       alert("Character length has to be between 8 - 128 digits. Please try again and type the number, do NOT spell it.");
       return false;
     }
-    //Lowercase selection
+    // Lowercase selection (if yes it adds lowerCaseArr to choiceArr)
     if (confirm("Would you like lowercase letters in the password?")) {
       choiceArr = choiceArr.concat(lowerCaseArr);
     }
-    // Uppercase selection
+    // Uppercase selection (if yes it adds upperCaseArr to choiceArr)
     if (confirm("Would you like uppercase letters in the password?")) {
       choiceArr = choiceArr.concat(upperCaseArr);
     }
-    // Special characters selection
+    // Special characters selection (if yes it adds specialCharArr to choiceArr)
     if (confirm("Would you like special characters in the password?")) {
       choiceArr = choiceArr.concat(specialCharArr);
     }
-    // Numbers selection
+    // Numbers selection (if yes it adds numberArr to choiceArr)
     if (confirm("Would you like numbers in the password?")) {
       choiceArr = choiceArr.concat(numberArr);
     }

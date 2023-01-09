243aff9a465e92f36fd0c83f75292b2c09ba533b
DomenicoBarb <domenicobarberi585@gmail.com>
2023-01-09 00:04:55 -0500

    organized code in more logical order

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ let lowerCaseArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
 let upperCaseArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 let numberArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-// Generate Button
+// Step 1 (Generates Button)
 let generateBtn = document.querySelector("#generate");
 
-// When "Clicked" run writePassword
+// Step 2 (When "Clicked" run writePassword)
 generateBtn.addEventListener("click", writePassword);
 
-// Displays new password on screen (in on-screen box)
+// Step 3 [Displays new password on screen (in on-screen box)]
 function writePassword() {
   // Calls getPrompts
   let correctChoicePrompt = getPrompts(); // will return true or false
@@ -28,22 +28,7 @@ function writePassword() {
   }
 }
 
-// Password Generation
-function generatePassword() {
-  // resets password value (defaults to an empty string)
-  let password = "";
-  // loop will run until i = the same value as characterLength
-  for(let i = 0; i < characterLength; i++) {
-    // random character selection math, math.floor for an integer (math.random creates random floating point value) giving random value between selected length
-    let randomIndex = Math.floor(Math.random() * choiceArr.length);
-    // starts with default password value and adds new randomIndex (random symbol from that array)
-    password = password + choiceArr[randomIndex];
-  }
-  // display generated password
-  return password;
-}
-
-// Prompt the user for the password criteria
+// Step 4 (Prompt the user for the password criteria)
 function getPrompts() {
   // resets choice array (defaults to empty)
   choiceArr = [];
@@ -71,4 +56,19 @@ function getPrompts() {
       choiceArr = choiceArr.concat(numberArr);
     }
     return true;
+}
+
+// Step 5 (Password Generation)
+function generatePassword() {
+  // resets password value (defaults to an empty string)
+  let password = "";
+  // loop will run until i = the same value as characterLength
+  for(let i = 0; i < characterLength; i++) {
+    // random character selection math, math.floor for an integer (math.random creates random floating point value) giving random value between selected length
+    let randomIndex = Math.floor(Math.random() * choiceArr.length);
+    // starts with default password value and adds new randomIndex (random symbol from that array)
+    password = password + choiceArr[randomIndex];
+  }
+  // display generated password
+  return password;
 }
